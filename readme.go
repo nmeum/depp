626384f3ad4443d11c7d77348e3b067bbf1cb782
Sören Tempel <soeren+git@soeren-tempel.net>
2020-11-15 22:12:38 +0100

    readme.go: Initialize

---
+++ readme.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/nmeum/depp/gitweb"
+)
+
+const renderScript = "git-readme-render"
+
+func runWithInput(cmd *exec.Cmd, input string) (string, error) {
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return "", err
+	}
+
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, input)
+	}()
+
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
+
+func renderReadme(repo *gitweb.Repo) (string, error) {
+	fp := filepath.Join(repo.Path, renderScript)
+	renderer, err := exec.LookPath(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", nil
+		}
+
+		return "", err
+	}
+
+	cmd := exec.Command(renderer)
+	readme, err := repo.Readme()
+	if err != nil {
+		return "", err
+	}
+
+	return runWithInput(cmd, readme)
+}
